f128a0894d6fd928e52eb42366fc9a404bafd318
MeytikhanyanHovsep <meytikhanyan.hovsep@gmail.com>
2026-01-20 12:14:54 +0400

    test

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,19 @@ export default defineConfig({
         tailwindcss(),
 
     ],
-
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                blog: resolve(__dirname, 'blog.html'),
+                blogArticle: resolve(__dirname, 'blog-article.html'),
+                contacts: resolve(__dirname, 'contacts.html'),
+                delivery: resolve(__dirname, 'delevery.html'), // проверьте опечатку в названии файла
+                product: resolve(__dirname, 'product.html'),
+                services: resolve(__dirname, 'services.html'),
+                stone: resolve(__dirname, 'stone-catalog.html'),
+            },
+        },
+    },
 });
