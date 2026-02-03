05cd186f9981f856c09252be93c8f41197ef8c28
MeytikhanyanHovsep <meytikhanyan.hovsep@gmail.com>
2026-02-03 14:04:26 +0400

    fixed bug of top-arrow-button

--- src/main.js
+++ src/main.js
@@ -37,7 +37,7 @@ document.addEventListener('alpine:init', () => {
         x-transition:leave="transition ease-in duration-200"
         x-transition:leave-end="opacity-0" 
         @click="window.scrollTo({ top: 0, behavior: 'smooth' }); showButton = false"
-        class="cursor-pointer z-50 bottom-40 max-lg:bottom-[130px] max-md:right-5 max-[1520px]:right-20  right-[calc((100%-1420px)/4)] [1520px]:translate-x-1/2 fixed w-[60px] object-contain"
+        class="cursor-pointer z-50 bottom-40 max-lg:bottom-[130px] max-md:right-5 max-[1520px]:right-20  right-[calc((100%-1420px)/4)] shadow-sm rounded-full fixed w-[60px] object-contain"
         alt="Top" />
 
 </div>
